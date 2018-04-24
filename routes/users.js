fbffa5eae1f295fd3fb8d5b32ce62ed42e5d32fb
liuliu <liuliu@xiaoliuliudexiao.local>
2018-04-24 23:42:53 +0800

    add model user and change route users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* post users info. */
+router.post('/login', function (req, res, next) {
+    console.log(req.body);
+    let param = {
+        userName: req.body.userName,
+        userPwd: req.body.userPwd
+    };
+    User.findOne(param, function (err, doc) {
+        if (err) {
+            res.json({
+                code: '900',
+                msg: err.message || '服务器错误'
+            })
+        } else {
+            if (doc) {
+                res.json({
+                    code: '000',
+                    msg: '',
+                    result: {
+                        userName: doc.userName
+                    }
+                });
+            }
+        }
+    });
 });
 
 module.exports = router;
